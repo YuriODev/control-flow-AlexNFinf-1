98d8860e25797792fe9dc95a67d66ced47bd3a30
Alex (NFinf) <121462320+AlexNFinf@users.noreply.github.com>
2024-05-24 09:09:12 +0000

    kajanan

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,9 @@
 # Your solution to Exercise 1
-
+alex = int(input("What's the age of Alex? "))
+tatyana = int(input("What's the age of Tatyana? "))
+if alex > tatyana:
+    print("Alex is the eldest.")
+elif alex == tatyana:
+    print("Alex and Tatyana are of the same age.")
+else:
+    print("Tatyana is the eldest.")
